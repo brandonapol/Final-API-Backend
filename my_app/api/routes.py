4a48aef821f33f6b172d3255748eea99547c1ba5
Brandon Apol <brandonapol@cedarville.edu>
2021-07-22 15:31:18 -0800

    added send_file function

--- my_app/api/routes.py
+++ my_app/api/routes.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, request, jsonify, redirect
+from flask import Blueprint, request, jsonify, redirect, send_file
 
 api = Blueprint('api',__name__, url_prefix='/api')
 
 @api.route('/getdata')
 def getdata():
-    return {'yee': 'haw'}
+    return send_file('../../test.txt')
     # return redirect('www.google.com')
 
 
