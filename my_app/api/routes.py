50fa283b922da3f57759bedc58534dd52948d8b5
Brandon Apol <brandonapol@cedarville.edu>
2021-07-26 19:33:14 -0800

    ready for launch

--- my_app/api/routes.py
+++ my_app/api/routes.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, request, jsonify, redirect, send_file
+import shutil
 
 api = Blueprint('api',__name__, url_prefix='/api')
 
 @api.route('/getdata')
 def getdata():
-    return send_file('../../test.txt')
-    # return redirect('www.google.com')
-
-
-# CREATEMOREROUTES
+    # var_1 = request.args.get('yee')
+    # print(var_1)
+    # folder = os.listdir('/Users/brandonapol/Documents/coding_temple_rangers_63/this_is_the_end/final_flask_app/test.txt')
+    # folder = os.path.join(folder, 'New Folder')
+    my_zip = shutil.make_archive('my_zip', 'zip', '/Users/brandonapol/Documents/coding_temple_rangers_63/this_is_the_end/template')
+    # You need to go back and undo this!
+    # #This works
+    return send_file(my_zip, as_attachment=True)
---
+++ my_app/file_changer/file_writer.py
@@ -0,0 +1,4 @@
+# take inputs
+# assign inputs to variables at the end of each template file
+# f.open('a') or whatever in each python template file what you want to assign the column names to be
+# this way each python file will 
